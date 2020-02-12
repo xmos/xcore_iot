2e5a5d8330a6fb2b6558848d1e8cb1d5c4272d10
Jeremiah McCarthy <jeremiahmccarthy@xmos.com>
2020-02-12 16:06:51 -0500

    Add cmake files
    Add cmake files for lib_rtos_support, lib_soc, FreeRTOS-Plus-TCP, and
    wfm example.

---
+++ lib_soc/src/peripherals/bitstream/spi_master_dev/bitstream_spimaster.cmake
@@ -0,0 +1,16 @@
+set(DEVICE_NAME LIB_SOC_SPIMASTER)
+set(DEVICE_ADD_COMPILER_FLAGS "")
+set(DEVICE_XC_SRCS
+        "src/peripherals/bitstream/spi_master_dev/spi_master_dev.xc"
+        "src/peripherals/bitstream/spi_master_dev/spi_fast/spi_fast.xc")
+set(DEVICE_C_SRCS
+        "")
+set(DEVICE_ASM_SRCS
+        "")
+set(DEVICE_INCLUDES
+        "src/peripherals/bitstream/spi_master_dev"
+        "src/peripherals/bitstream/spi_master_dev/spi_fast")
+set(DEVICE_DEPENDENT_MODULES
+        "")
+set(DEVICE_OPTIONAL_HEADERS
+        "")
