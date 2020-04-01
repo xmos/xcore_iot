3120383c5965f2a2507d5687ef289821b658da56
Mike Bruno <mikeb@xmos.com>
2020-04-01 10:03:38 -0400

    Move RPC from example app into software services

--- lib_soc/src/sw_services/rpc/sw_svc_rpc.cmake
+++ lib_soc/src/sw_services/rpc/sw_svc_rpc.cmake
@@ -3,11 +3,11 @@ set(SW_SVC_ADD_COMPILER_FLAGS "")
 set(SW_SVC_XC_SRCS
         "")
 set(SW_SVC_C_SRCS
-        "")
+        "src/sw_services/rpc/src/rpc.c")
 set(SW_SVC_ASM_SRCS
         "")
 set(SW_SVC_INCLUDES
-        "")
+        "src/sw_services/rpc/api")
 set(SW_SVC_DEPENDENT_MODULES
         "")
 set(SW_SVC_OPTIONAL_HEADERS
