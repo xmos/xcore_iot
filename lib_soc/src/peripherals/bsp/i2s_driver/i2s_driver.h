ddf1443a8fe303f8014ddbb4a354e8d5b5e51e68
Jeremiah McCarthy <jeremiahmccarthy@xmos.com>
2020-07-24 16:53:34 -0400

    Add comments to i2c, i2s, and mic drivers

--- lib_soc/src/peripherals/bsp/i2s_driver/i2s_driver.h
+++ lib_soc/src/peripherals/bsp/i2s_driver/i2s_driver.h
@@ -1,10 +1,23 @@
-// Copyright (c) 2019, XMOS Ltd, All rights reserved
+// Copyright (c) 2019-2020, XMOS Ltd, All rights reserved
 
 #ifndef I2S_DRIVER_H_
 #define I2S_DRIVER_H_
 
 #include "soc.h"
 
+/**
+ * Initialize the i2s device
+ *
+ * \param[in]     device_id      ID of i2s device to use
+ * \param[in]     rx_desc_count  Maximum number of rx descriptors
+ * \param[in]     rx_buf_size    Size of each of rx descriptor buffer
+ * \param[in]     tx_desc_count  Maximum number of tx descriptors
+ * \param[in]     app_data  	 App data pointer
+ * \param[in]     isr_core       FreeRTOS core to handle interrupts from device pointer
+ * \param[in]     isr            ISR to handle interrupts from device
+ *
+ * \returns       Initialized i2s device
+ */
 soc_peripheral_t i2s_driver_init(
         int device_id,
         int rx_desc_count,
--- lib_soc/src/peripherals/bsp/micarray_driver/micarray_driver.h
+++ lib_soc/src/peripherals/bsp/micarray_driver/micarray_driver.h
@@ -1,10 +1,23 @@
-// Copyright (c) 2019, XMOS Ltd, All rights reserved
+// Copyright (c) 2019-2020, XMOS Ltd, All rights reserved
 
 #ifndef MICARRAY_DRIVER_H_
 #define MICARRAY_DRIVER_H_
 
 #include "soc.h"
 
+/**
+ * Initialize the micarray device
+ *
+ * \param[in]     device_id      ID of micarray device to use
+ * \param[in]     rx_desc_count  Maximum number of rx descriptors
+ * \param[in]     rx_buf_size    Size of each of rx descriptor buffer
+ * \param[in]     tx_desc_count  Maximum number of tx descriptors
+ * \param[in]     app_data  	 App data pointer
+ * \param[in]     isr_core       FreeRTOS core to handle interrupts from device
+ * \param[in]     isr            ISR to handle interrupts from device
+ *
+ * \returns       Initialized micarray device
+ */
 soc_peripheral_t micarray_driver_init(
         int device_id,
         int rx_desc_count,
