0eae750dd64208ec853ad8576ab1b9b3092d2a5e
Keith Moulton <keithm@xmos.com>
2020-09-25 14:40:38 -0400

    added build script

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#**************************
+# Build bare-metal examples
+#**************************
+declare -a bare_metal_examples=(
+    "cifar10"
+    "hello_world"
+    "micro_speech"
+    "mobilenet_v1"
+    )
+ 
+for example in ${bare_metal_examples[@]}; do
+    (cd examples/bare-metal/${example}; mkdir -p build) 
+    (cd examples/bare-metal/${example}/build; cmake ../ ; make install) 
+done
+
+#**************************
+# Build FreeRTOS examples
+#**************************
+BOARD="-DBOARD=XCORE-AI-EXPLORER"
+
+declare -a freertos_examples=(
+    "cifar10"
+    "explorer_board"
+    "independent_tiles"
+    "iot_aws"
+    "person_detection"
+    )
+
+for example in ${freertos_examples[@]}; do
+    (cd examples/freertos/${example}; mkdir -p build) 
+    (cd examples/freertos/${example}/build; cmake ../ ${BOARD}; make) 
+done
+
+#**************************
+# Run bare-metal tests
+#**************************
