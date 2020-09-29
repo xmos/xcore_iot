6d50a37dbd469371b4b56709ed76bf1934d87f07
Keith Moulton <kamoulton@gmail.com>
2020-09-29 19:33:59 -0400

    set MKFS_VFAT_PATH based on uname

--- examples/freertos/explorer_board/filesystem_support/create_fs.sh
+++ examples/freertos/explorer_board/filesystem_support/create_fs.sh
@@ -1,25 +1,32 @@
+# Get unix name for determining OS
+UNAME=$(uname)
+
 # Create an empty 1 MiB file
 dd if=/dev/zero of=fat.fs bs=1024 count=1024
 
 if [ "$UNAME" == "Linux" ] ; then
+    MKFS_VFAT_PATH=/sbin
     sudo umount -q fat_mnt
 elif [ "$UNAME" == "Darwin" ] ; then
+    MKFS_VFAT_PATH=/usr/local/sbin
     hdiutil detach fat_mnt
 fi
 
 # Create an empty FAT filesystem in it
-mkfs.vfat -v -F12 -s1 -S4096 -n xcore_filesystem fat.fs
+$MKFS_VFAT_PATH/mkfs.vfat -v -F12 -s1 -S4096 -n xcore_filesystem fat.fs
 
 mkdir -p fat_mnt
 
 WF200_FW=$XMOS_AIOT_SDK_PATH/modules/lib_soc/src/peripherals/bsp/wf200_driver/thirdparty/wfx-firmware/wfm_wf200_C0.sec
 
+# Mount the filesystem
 if [ "$UNAME" == "Linux" ] ; then
     sudo mount -o loop fat.fs fat_mnt
 elif [ "$UNAME" == "Darwin" ] ; then
     hdiutil attach -imagekey diskimage-class=CRawDiskImage -mountpoint fat_mnt fat.fs
 fi
 
+# Copy files into filesystem
 sudo mkdir fat_mnt/firmware
 sudo mkdir fat_mnt/crypto
 sudo mkdir fat_mnt/server
@@ -36,10 +43,12 @@ if [ ! -f networks.dat ]; then
 fi
 sudo cp networks.dat fat_mnt/wifi
 
+# Unmount the filesystem
 if [ "$UNAME" == "Linux" ] ; then
     sudo umount fat_mnt
 elif [ "$UNAME" == "Darwin" ] ; then
     hdiutil detach fat_mnt
 fi
 
+# Cleanup
 sudo rm -rf fat_mnt
