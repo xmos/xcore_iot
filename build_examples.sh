dba833f11fc8a7f706c68976bfd7911269fff0a1
Jeremiah McCarthy <jeremiahmccarthy@xmos.com>
2020-11-11 13:08:42 -0500

    Update microspeech for Osprey

--- build_examples.sh
+++ build_examples.sh
@@ -1,20 +1,43 @@
 #!/bin/bash
 set -e
 
+declare -a boards=(
+    "XCORE-AI-EXPLORER"
+    "OSPREY-BOARD"
+    )
+
 #**************************
 # Build bare-metal examples
 #**************************
 declare -a bare_metal_examples=(
+    "micro_speech"
+    )
+
+for board in ${boards[@]}; do
+    for example in ${bare_metal_examples[@]}; do
+        echo '******************************************************'
+        echo '* Building' ${example} 'for' ${board}
+        echo '******************************************************'
+        (cd examples/bare-metal/${example}; rm -rf build_${board})
+        (cd examples/bare-metal/${example}; mkdir -p build_${board})
+        (cd examples/bare-metal/${example}/build_${board}; cmake ../ -DBOARD=${board}; make install)
+    done
+done
+
+#*************************************************
+# Build bare-metal XCORE-AI-EXPLORER only examples
+#*************************************************
+BOARD=XCORE-AI-EXPLORER
+declare -a bare_metal_examples=(
     "cifar10"
     "hello_world"
-    "micro_speech"
     "mobilenet_v1"
     )
 
 for example in ${bare_metal_examples[@]}; do
-    echo '***************************'
-    echo '* Building' ${example}
-    echo '***************************'
+    echo '******************************************************'
+    echo '* Building' ${example} 'for' ${BOARD}
+    echo '******************************************************'
     (cd examples/bare-metal/${example}; rm -rf build)
     (cd examples/bare-metal/${example}; mkdir -p build)
     (cd examples/bare-metal/${example}/build; cmake ../ ; make install)
@@ -23,11 +46,6 @@ done
 #**************************
 # Build FreeRTOS examples
 #**************************
-declare -a boards=(
-    "XCORE-AI-EXPLORER"
-    "OSPREY-BOARD"
-    )
-
 declare -a freertos_examples=(
     "cifar10"
     )
@@ -67,7 +85,7 @@ declare -a freertos_examples=(
 
 for example in ${freertos_examples[@]}; do
     echo '******************************************************'
-    echo '* Building' ${example} 'for' ${board}
+    echo '* Building' ${example} 'for' ${BOARD}
     echo '******************************************************'
     (cd examples/freertos/${example}; rm -rf build)
     (cd examples/freertos/${example}; mkdir -p build)
