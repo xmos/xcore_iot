655ee2c4b35abadc09b7fa5eec69642065f3128b
Keith Moulton <keithm@xmos.com>
2020-11-30 16:11:54 -0500

    now removes build folders as part of install build

--- install_tools.sh
+++ install_tools.sh
@@ -7,6 +7,7 @@ echo "****************************"
 echo "* Building libtflite2xcore *"
 echo "****************************"
 LIB_FLEXBUFFERS_DIR=${AI_TOOLS_DIR}/utils/lib_flexbuffers
+(cd ${LIB_FLEXBUFFERS_DIR}; rm -rf build) 
 (cd ${LIB_FLEXBUFFERS_DIR}; mkdir -p build) 
 (cd ${LIB_FLEXBUFFERS_DIR}/build; cmake ../ ; make install) 
 
@@ -14,6 +15,7 @@ echo "*******************************"
 echo "* Building xcore_interpreters *"
 echo "******************************"
 XCORE_INTERPRETERS_DIR=${AI_TOOLS_DIR}/xcore_interpreters/python_bindings
+(cd ${XCORE_INTERPRETERS_DIR}; rm -rf build) 
 (cd ${XCORE_INTERPRETERS_DIR}; mkdir -p build) 
 (cd ${XCORE_INTERPRETERS_DIR}/build; cmake ../ ; make install) 
 
