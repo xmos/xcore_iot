e134d45b661d77230e326647f213d50d0b2af226
Michael Bruno <mikeb@xmos.com>
2021-01-12 23:42:23 -0500

    Document the API for the RTOS intertile driver

--- modules/legacy_compat/debug_print.h
+++ modules/legacy_compat/debug_print.h
@@ -2,6 +2,10 @@
 #ifndef LEGACY_COMPAT_DEBUG_PRINTF
 #define LEGACY_COMPAT_DEBUG_PRINTF
 
+#ifndef RTOS_DEBUG_PRINTF_REMAP
+#define RTOS_DEBUG_PRINTF_REMAP 1
+#endif
+
 #include "rtos_printf.h"
 
 #endif /* LEGACY_COMPAT_DEBUG_PRINTF */
