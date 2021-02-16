2fc9017ffc42041d81c7f9a66d442c3de6d493cf
Keith Moulton <keithm@xmos.com>
2021-02-16 16:48:26 -0500

    now properly sets PATH env var

--- install.sh
+++ install.sh
@@ -15,9 +15,8 @@ else
     echo "* Building libtflite2xcore *"
     echo "****************************"
     LIB_FLEXBUFFERS_DIR=${AI_TOOLS_DIR}/utils/lib_flexbuffers
-    (cd ${LIB_FLEXBUFFERS_DIR}; rm -rf build) 
-    (cd ${LIB_FLEXBUFFERS_DIR}; mkdir -p build) 
-    (cd ${LIB_FLEXBUFFERS_DIR}/build; cmake ../ ; make install) 
+    (cd ${LIB_FLEXBUFFERS_DIR}; mkdir -p build)
+    (cd ${LIB_FLEXBUFFERS_DIR}/build; cmake ../ ; make install)
 
     ADF_DIR=${XMOS_AIOT_SDK_PATH}/modules/adf
 
@@ -25,18 +24,24 @@ else
     echo "* Building xcore_interpreters *"
     echo "******************************"
     XCORE_INTERPRETERS_DIR=${ADF_DIR}/xcore_interpreters/python_bindings
-    (cd ${XCORE_INTERPRETERS_DIR}; rm -rf build) 
-    (cd ${XCORE_INTERPRETERS_DIR}; mkdir -p build) 
-    (cd ${XCORE_INTERPRETERS_DIR}/build; cmake ../ ; make install) 
+    (cd ${XCORE_INTERPRETERS_DIR}; mkdir -p build)
+    (cd ${XCORE_INTERPRETERS_DIR}/build; cmake ../ ; make install)
 
     echo "**********************************"
     echo "* Installing Python requirements *"
     echo "**********************************"
-    (pip install -r requirements.txt)
+    pip install -r requirements.txt
 
     echo "**************************************"
     echo "* Updating PATH environment variable *"
     echo "**************************************"
-    MODEL_RUNNER_GENERATOR_PATH="${XMOS_AIOT_SDK_PATH}/modules/adf/tools/generate"
-    export PATH="${MODEL_RUNNER_GENERATOR_PATH}:${PATH}"
+    cd $CONDA_PREFIX
+    mkdir -p ./etc/conda/activate.d
+    touch ./etc/conda/activate.d/env_vars.sh
+    echo "PATH=${XMOS_AIOT_SDK_PATH}/modules/adf/tools/generate:${PATH}" > ./etc/conda/activate.d/env_vars.sh
+    cd ..
+    echo ""
+    echo "Deactivate and activate your environment to complete the install:"
+    echo "$ conda deactivate"
+    echo "$ conda activate ${CONDA_PREFIX}"
 fi
