fc77d5227e166cab24cb634ecee84fd70ff4a7b1
Keith Moulton <keithm@xmos.com>
2021-04-05 12:30:19 -0400

    fixed some build issues, exampls still not all building

--- examples/freertos/cifar10/src/model_runner/cifar10_model_runner.cc
+++ examples/freertos/cifar10/src/model_runner/cifar10_model_runner.cc
@@ -15,26 +15,29 @@ static resolver_t *resolver = nullptr;
 
 static profiler_t *profiler = nullptr;
 
-__attribute__((fptrgroup("model_runner_resolver_get_fptr_grp")))
-void cifar10_resolver_get(void **v_resolver)
-{
+__attribute__((fptrgroup("model_runner_resolver_get_fptr_grp"))) void
+cifar10_resolver_get(void **v_resolver) {
   // Set up op resolver
   //   This pulls in all the operation implementations we need.
   if (resolver == nullptr) {
     resolver = &resolver_s;
     resolver->AddPad();
     resolver->AddSoftmax();
-    resolver->AddCustom(tflite::ops::micro::xcore::FullyConnected_8_OpCode, tflite::ops::micro::xcore::Register_FullyConnected_8());
-    resolver->AddCustom(tflite::ops::micro::xcore::Conv2D_Deep_OpCode, tflite::ops::micro::xcore::Register_Conv2D_Deep());
-    resolver->AddCustom(tflite::ops::micro::xcore::MaxPool2D_OpCode, tflite::ops::micro::xcore::Register_MaxPool2D());
-    resolver->AddCustom(tflite::ops::micro::xcore::Conv2D_Shallow_OpCode, tflite::ops::micro::xcore::Register_Conv2D_Shallow());
+    resolver->AddCustom(tflite::ops::micro::xcore::FullyConnected_8_OpCode,
+                        tflite::ops::micro::xcore::Register_FullyConnected_8());
+    resolver->AddCustom(tflite::ops::micro::xcore::Conv2D_Deep_OpCode,
+                        tflite::ops::micro::xcore::Register_Conv2D_Deep());
+    resolver->AddCustom(tflite::ops::micro::xcore::MaxPool2D_OpCode,
+                        tflite::ops::micro::xcore::Register_MaxPool2D());
+    resolver->AddCustom(tflite::ops::micro::xcore::Conv2D_Shallow_OpCode,
+                        tflite::ops::micro::xcore::Register_Conv2D_Shallow());
   }
 
   *v_resolver = static_cast<void *>(resolver);
 }
 
-__attribute__((fptrgroup("model_runner_profiler_get_fptr_grp")))
-void cifar10_profiler_get(void **v_profiler) {
+__attribute__((fptrgroup("model_runner_profiler_get_fptr_grp"))) void
+cifar10_profiler_get(void **v_profiler) {
 #ifndef NDEBUG
   if (profiler == nullptr) {
     // Set up profiling
@@ -47,8 +50,8 @@ void cifar10_profiler_get(void **v_profiler) {
   *v_profiler = static_cast<void *>(profiler);
 }
 
-__attribute__((fptrgroup("model_runner_profiler_reset_fptr_grp")))
-void cifar10_profiler_reset() {
+__attribute__((fptrgroup("model_runner_profiler_reset_fptr_grp"))) void
+cifar10_profiler_reset() {
   if (profiler) {
     profiler->ClearEvents();
   }
@@ -56,11 +59,11 @@ void cifar10_profiler_reset() {
 
 #ifndef NDEBUG
 
-__attribute__((fptrgroup("model_runner_profiler_times_get_fptr_grp")))
-void cifar10_profiler_times_get(uint32_t *count, const uint32_t **times) {
+__attribute__((fptrgroup("model_runner_profiler_durations_get_fptr_grp"))) void
+cifar10_profiler_durations_get(uint32_t *count, const uint32_t **duration) {
   if (profiler) {
     *count = profiler->GetNumEvents();
-    *times = profiler->GetEventDurations();
+    *duration = profiler->GetEventDurations();
   }
 }
 
@@ -74,5 +77,5 @@ void cifar10_model_runner_create(model_runner_t *ctx, void *buffer) {
   ctx->resolver_get_fun = &cifar10_resolver_get;
   ctx->profiler_get_fun = &cifar10_profiler_get;
   ctx->profiler_reset_fun = &cifar10_profiler_reset;
-  ctx->profiler_times_get_fun = &cifar10_profiler_times_get;
+  ctx->profiler_durations_get_fun = &cifar10_profiler_durations_get;
 }
