f6555365942f08dceff56b0a9c749d29a320fb3e
Michael Bruno <mikeb@xmos.com>
2021-06-23 11:12:06 -0400

    Update the SPI slave driver's Doxygen comments

--- modules/rtos/drivers/spi/api/rtos_spi_slave.h
+++ modules/rtos/drivers/spi/api/rtos_spi_slave.h
@@ -17,14 +17,6 @@
 #include "rtos/osal/api/rtos_osal.h"
 #include "rtos/drivers/rpc/api/rtos_driver_rpc.h"
 
-///**
-// * The maximum number of bytes that a the RTOS SPI slave driver can receive from a master
-// * in a single write transaction.
-// */
-//#ifndef RTOS_SPI_SLAVE_BUF_LEN
-//#define RTOS_SPI_SLAVE_BUF_LEN 256
-//#endif
-
 /**
  * This attribute must be specified on all RTOS SPI slave callback functions
  * provided by the application.
@@ -39,9 +31,9 @@ typedef struct rtos_spi_slave_struct rtos_spi_slave_t;
 /**
  * Function pointer type for application provided RTOS SPI slave start callback functions.
  *
- * These callback functions are optionally called by an SPI slave driver's thread when it is first
+ * These callback functions are optionally called by a SPI slave driver's thread when it is first
  * started. This gives the application a chance to perform startup initialization from within the
- * driver's thread.
+ * driver's thread. It is a good place for the first call to spi_slave_xfer_prepare().
  *
  * \param ctx           A pointer to the associated SPI slave driver instance.
  * \param app_data      A pointer to application specific data provided
@@ -51,20 +43,18 @@ typedef struct rtos_spi_slave_struct rtos_spi_slave_t;
 typedef void (*rtos_spi_slave_start_cb_t)(rtos_spi_slave_t *ctx, void *app_data);
 
 /**
- * Function pointer type for application provided RTOS SPI slave transmit done callback functions.
+ * Function pointer type for application provided RTOS SPI slave transfer done callback functions.
  *
- * These callback functions are optionally called when an SPI slave driver instance is done transmitting data to
- * a master device. A buffer to the data sent and the actual number of bytes sent are provided to the callback.
+ * These callback functions are optionally called when a SPI slave driver instance is done transferring data with
+ * a master device.
  *
- * The application may want to use this, for example, if the buffer that was sent was malloc'd. This callback
- * can be used to free the buffer.
+ * An application can use this to be notified immediately when a transfer has completed. It can then call
+ * spi_slave_xfer_complete() with a timeout of 0 from within this callback to get the transfer results.
  *
  * \param ctx           A pointer to the associated SPI slave driver instance.
  * \param app_data      A pointer to application specific data provided
  *                      by the application. Used to share data between
  *                      this callback function and the application.
- * \param data          A pointer to the data transmitted to the master.
- * \param len           The number of bytes transmitted to the master from \p data.
  */
 typedef void (*rtos_spi_slave_xfer_done_cb_t)(rtos_spi_slave_t *ctx, void *app_data);
 
@@ -101,10 +91,61 @@ struct rtos_spi_slave_struct {
     rtos_osal_thread_t app_thread;
 };
 
+/**
+ * Prepares an RTOS SPI slave driver instance with buffers for subsequent transfers.
+ * Before this is called for the first time, any transfers initiated by a master device
+ * with result in all received data over MOSI being dropped, and all data sent over
+ * MISO being zeros.
+ *
+ * This only needs to be called when the buffers need to be changed. If all transfers
+ * will use the same buffers, then this only needs to be called once during initialization.
+ *
+ * \param ctx        A pointer to the SPI slave driver instance to use.
+ * \param rx_buf     The buffer to receive data into for any subsequent transfers.
+ * \param rx_buf_    The length in bytes of \p rx_buf. If the master transfers more than
+ *                   this during a single transfer, then the bytes that do not fit within
+ *                   \p rx_buf will be lost.
+ * \param tx_buf     The buffer to send data from for any subsequent transfers.
+ * \param tx_buf_len The length in bytes of \p tx_buf. If the master transfers more than
+ *                   this during a single transfer, zeros will be sent following the last
+ *                   byte \p tx_buf.
+ */
+void spi_slave_xfer_prepare(
+        rtos_spi_slave_t *ctx,
+        void *rx_buf,
+        size_t rx_buf_len,
+        void *tx_buf,
+        size_t tx_buf_len);
 
-
-void spi_slave_xfer_prepare(rtos_spi_slave_t *ctx, void *rx_buf, size_t rx_buf_len, void *tx_buf, size_t tx_buf_len);
-int spi_slave_xfer_complete(rtos_spi_slave_t *ctx, void **rx_buf, size_t *rx_len, void **tx_buf, size_t *tx_len, unsigned timeout);
+/**
+ * Waits for a SPI transfer to complete. Returns either when the timeout is reached, or
+ * when a transfer completes, whichever comes first. If a transfer does complete, then
+ * the buffers and the number of bytes read from or written to them are returned via
+ * the parameters.
+ *
+ * \param ctx     A pointer to the SPI slave driver instance to use.
+ * \param rx_buf  The receive buffer used for the completed transfer. This is set by
+ *                the function upon completion of a transfer.
+ * \param rx_len  The number of bytes written to rx_buf. This is set by the function
+ *                upon completion of a transfer.
+ * \param tx_buf  The transmit buffer used for the completed transfer. This is set by
+ *                the function upon completion of a transfer.
+ * \param tx_len  The number of bytes sent from tx_buf. This is set by the function
+ *                upon completion of a transfer.
+ * \param timeout The number of RTOS ticks to wait before the next transfer is complete.
+ *                When called from within the "xfer_done" callback, this should be 0.
+ *
+ * \retval  0 if a transfer completed. All buffers and lengths are set in this case.
+ * \retval -1 if no transfer completed before the timeout expired. No buffers or lengths
+ *            are returned in this case.
+ */
+int spi_slave_xfer_complete(
+        rtos_spi_slave_t *ctx,
+        void **rx_buf,
+        size_t *rx_len,
+        void **tx_buf,
+        size_t *tx_len,
+        unsigned timeout);
 
 /**
  * Starts an RTOS SPI slave driver instance. This must only be called by the tile that
@@ -117,13 +158,12 @@ int spi_slave_xfer_complete(rtos_spi_slave_t *ctx, void **rx_buf, size_t *rx_len
  *                          the callback functions.
  * \param start             The callback function that is called when the driver's
  *                          thread starts. This is optional and may be NULL.
- * \param rx                The callback function to receive data from the bus master.
- * \param tx_start          The callback function to transmit data to the bus master.
- * \param tx_done           The callback function that is notified when transmits are
+ * \param xfer_done         The callback function that is notified when transfers are
  *                          complete. This is optional and may be NULL.
  * \param interrupt_core_id The ID of the core on which to enable the SPI interrupt.
  * \param priority          The priority of the task that gets created by the driver to
- *                          call the callback functions.
+ *                          call the callback functions. If both callback functions are
+ *                          NULL, then this is unused.
  */
 void rtos_spi_slave_start(
         rtos_spi_slave_t *spi_slave_ctx,
@@ -142,11 +182,13 @@ void rtos_spi_slave_start(
  * \param io_core_mask  A bitmask representing the cores on which the low level SPI I/O thread
  *                      created by the driver is allowed to run. Bit 0 is core 0, bit 1 is core 1,
  *                      etc.
- * \param p_scl         The port containing SCL. This must be a 1-bit port and
- *                      different than \p p_sda.
- * \param p_sda         The port containing SDA. This must be a 1-bit port and
- *                      different than \p p_scl.
- * \param device_addr   The 7-bit address of the slave device.
+ * \param clock_block   The clock block to use for the SPI slave.
+ * \param cpol          The clock polarity to use.
+ * \param cpha          The clock phase to use.
+ * \param p_sclk        The SPI slave's SCLK port. Must be a 1-bit port.
+ * \param p_mosi        The SPI slave's MOSI port. Must be a 1-bit port.
+ * \param p_miso        The SPI slave's MISO port. Must be a 1-bit port.
+ * \param p_cs          The SPI slave's CS port. Must be a 1-bit port.
  */
 void rtos_spi_slave_init(
         rtos_spi_slave_t *spi_slave_ctx,
