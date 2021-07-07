1d8c3f196d6f0792d14de76c638ad8a4d42d3d7e
Jeremiah McCarthy <jeremiahmccarthy@xmos.com>
2021-07-07 10:20:56 -0400

    Change platform.h behavour for xc files
    cmake 3.20 appears to cause issues for the ON_TILE macro, but only
    affecting xc files.
    
    Builds on Linux.

--- modules/multitile_support/platform.h
+++ modules/multitile_support/platform.h
@@ -3,11 +3,15 @@
 #ifndef MULTITILE_SUPPORT_PLATFORM_H
 #define MULTITILE_SUPPORT_PLATFORM_H
 
-#ifdef THIS_XCORE_TILE
-#define ON_TILE(t) (THIS_XCORE_TILE == (t))
+#if __XC__
+#ifndef THIS_XCORE_TILE
+#define ON_TILE(t) (t == t)
 #else
-#error "THIS_XCORE_TILE not defined!"
+#define ON_TILE(t) (THIS_XCORE_TILE == (t))
 #endif
+#else /* __XC__ */
+#define ON_TILE(t) (!defined(THIS_XCORE_TILE) || THIS_XCORE_TILE == (t))
+#endif /* __XC__ */
 
 #include_next <platform.h>
 
