cab9885a4adaf8fb8d5d5744ca858ddb81e63bab
Keith Moulton <keithm@xmos.com>
2021-07-21 15:18:22 -0400

    now using new tflite-micro submodule

--- modules/aif/test/xcore_all_ops_firmware/src/model_runner.cc
+++ modules/aif/test/xcore_all_ops_firmware/src/model_runner.cc
@@ -6,7 +6,6 @@
 #include "tensorflow/lite/micro/kernels/xcore/xcore_interpreter.h"
 #include "tensorflow/lite/micro/kernels/xcore/xcore_profiler.h"
 #include "tensorflow/lite/micro/micro_error_reporter.h"
-#include "tensorflow/lite/version.h"
 #include "xcore_all_ops_resolver.h"
 
 // shorthand typedefs
@@ -30,37 +29,46 @@ static const model_t *model = nullptr;
 // static buffer for interpreter_t class allocation
 uint8_t interpreter_buffer[sizeof(interpreter_t)];
 
-void model_runner_tensor_bytes_get(int index, void **bytes, size_t *size) {
+void model_runner_tensor_bytes_get(int index, void **bytes, size_t *size)
+{
   TfLiteTensor *tensor = interpreter->tensor(index);
-  if (tensor != nullptr) {
+  if (tensor != nullptr)
+  {
     *bytes = tensor->data.raw;
     *size = tensor->bytes;
-  } else {
+  }
+  else
+  {
     *bytes = nullptr;
     *size = 0;
   }
 }
 
-ModelRunnerStatus model_runner_invoke() {
+ModelRunnerStatus model_runner_invoke()
+{
   // reset the profiler
-  if (profiler) {
+  if (profiler)
+  {
     profiler->ClearEvents();
   }
 
   // Run inference, and report any error
   TfLiteStatus invoke_status = interpreter->Invoke();
 
-  if (invoke_status != kTfLiteOk) {
+  if (invoke_status != kTfLiteOk)
+  {
     return InvokeError;
   }
   return Ok;
 }
 
-void model_runner_reset(uint8_t *tensor_arena, size_t tensor_arena_size) {
-  if (interpreter) {
+void model_runner_reset(uint8_t *tensor_arena, size_t tensor_arena_size)
+{
+  if (interpreter)
+  {
     // delete existing interpreter
-    delete interpreter;  // NOTE: interpreter must be deleted before
-                         // resolver and reporter
+    delete interpreter; // NOTE: interpreter must be deleted before
+                        // resolver and reporter
     // need to memset the arena to 0
     memset(tensor_arena, 0, tensor_arena_size);
   }
@@ -70,40 +78,46 @@ ModelRunnerStatus model_runner_init(uint8_t *model_content,
                                     uint8_t *tensor_arena,
                                     size_t tensor_arena_size, uint8_t **input,
                                     size_t *input_size, uint8_t **output,
-                                    size_t *output_size) {
+                                    size_t *output_size)
+{
   // Set up logging
-  if (reporter == nullptr) {
+  if (reporter == nullptr)
+  {
     reporter = &error_reporter_s;
   }
 
 #ifndef NDEBUG
   // Set up profiling
-  if (profiler == nullptr) {
+  if (profiler == nullptr)
+  {
     profiler = &profiler_s;
   }
 #endif
 
   // Set up op resolver
   //   This pulls in all the operation implementations we need.
-  if (resolver == nullptr) {
+  if (resolver == nullptr)
+  {
     resolver = &resolver_s;
   }
 
   // Map the model into a usable data structure. This doesn't involve any
   // copying or parsing, it's a very lightweight operation.
   model = tflite::GetModel(model_content);
-  if (model->version() != TFLITE_SCHEMA_VERSION) {
+  if (model->version() != TFLITE_SCHEMA_VERSION)
+  {
     return ModelVersionError;
   }
 
   // Build an interpreter to run the model with
   interpreter = new (interpreter_buffer)
       interpreter_t(model, *resolver, tensor_arena, tensor_arena_size, reporter,
-                    true, profiler);
+                    profiler);
 
   // Allocate memory from the tensor_arena for the model's tensors.
   TfLiteStatus allocate_tensors_status = interpreter->AllocateTensors();
-  if (allocate_tensors_status != kTfLiteOk) {
+  if (allocate_tensors_status != kTfLiteOk)
+  {
     return AllocateTensorsError;
   }
 
@@ -116,11 +130,15 @@ ModelRunnerStatus model_runner_init(uint8_t *model_content,
   return Ok;
 }
 
-void model_runner_profiler_times_get(uint32_t *count, const uint32_t **times) {
-  if (profiler) {
+void model_runner_profiler_times_get(uint32_t *count, const uint32_t **times)
+{
+  if (profiler)
+  {
     *count = profiler->GetNumEvents();
     *times = profiler->GetEventDurations();
-  } else {
+  }
+  else
+  {
     *count = 0;
     *times = static_cast<uint32_t *>(nullptr);
   }
