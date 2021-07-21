9566a493ad166e55b9be1feda68b2d1eb16071d7
Keith Moulton <keithm@xmos.com>
2021-07-21 15:18:22 -0400

    now using new tflite-micro submodule

--- modules/aif/ai_framework.cmake
+++ modules/aif/ai_framework.cmake
@@ -8,8 +8,8 @@ set(GEMMLOWP_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/gemmlowp")
 set(RUY_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/ruy")
 set(FLATBUFFERS_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/flatbuffers/include")
 set(FLATBUFFERS_SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/flatbuffers/src")
-set(TENSORFLOW_SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/tensorflow")
-set(TENSORFLOW_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/tensorflow")
+set(TFLITE_MICRO_SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/tflite-micro")
+set(TFLITE_MICRO_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/tflite-micro")
 
 set(LIB_NN_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/lib_nn")
 set(LIB_NN_ALT_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/lib_nn/lib_nn/api")
@@ -20,117 +20,138 @@ set(MODEL_RUNNER_DIR "${CMAKE_CURRENT_LIST_DIR}/model_runner")
 #********************************
 # TensorFlow Lite Micro sources
 #********************************
-set(TENSORFLOW_LITE_RUNTIME_SOURCES
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/c/common.c"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/core/api/error_reporter.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/core/api/flatbuffer_conversions.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/core/api/op_resolver.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/core/api/tensor_utils.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/kernels/kernel_util.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/kernels/internal/quantization_util.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/memory_helpers.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/micro_allocator.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/recording_micro_allocator.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/micro_error_reporter.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/micro_interpreter.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/micro_profiler.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/micro_utils.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/micro_string.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/simple_memory_allocator.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/recording_simple_memory_allocator.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/memory_planner/greedy_memory_planner.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/memory_planner/linear_memory_planner.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/fully_connected_common.cc"    
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/kernel_util.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/quantize_common.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/schema/schema_utils.cc"
+set(TFLITE_MICRO_RUNTIME_SOURCES
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/c/common.c"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/core/api/error_reporter.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/core/api/flatbuffer_conversions.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/core/api/op_resolver.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/core/api/tensor_utils.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/kernels/kernel_util.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/kernels/internal/quantization_util.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/flatbuffer_utils.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/memory_helpers.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/micro_allocator.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/micro_error_reporter.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/micro_graph.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/micro_interpreter.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/micro_profiler.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/micro_utils.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/micro_string.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/recording_micro_allocator.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/recording_simple_memory_allocator.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/simple_memory_allocator.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/memory_planner/greedy_memory_planner.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/memory_planner/linear_memory_planner.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/fully_connected_common.cc"    
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/kernel_util.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/quantize_common.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/schema/schema_utils.cc"
   )
 
 if (X86)
-  set(TENSORFLOW_LITE_RUNTIME_SOURCES
-    ${TENSORFLOW_LITE_RUNTIME_SOURCES}
+  set(TFLITE_MICRO_RUNTIME_SOURCES
+    ${TFLITE_MICRO_RUNTIME_SOURCES}
     "${FLATBUFFERS_SOURCE_DIR}/util.cpp"
-    "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/debug_log.cc"
-    "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/micro_time.cc"
+    "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/debug_log.cc"
+    "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/micro_time.cc"
     )
 else ()
-  set(TENSORFLOW_LITE_RUNTIME_SOURCES
-    ${TENSORFLOW_LITE_RUNTIME_SOURCES}
-    "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/xcore/debug_log.cc"
-    "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/xcore/micro_time.cc"
+  set(TFLITE_MICRO_RUNTIME_SOURCES
+    ${TFLITE_MICRO_RUNTIME_SOURCES}
+    "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/xcore/debug_log.cc"
+    "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/xcore/micro_time.cc"
     )
 endif ()
 
 #*************************************************
 # TensorFlow Lite Micro reference kernel sources
 #*************************************************
-set(TENSORFLOW_LITE_REFERENCE_OPERATOR_SOURCES
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/activations.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/add.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/arg_min_max.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/ceil.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/circular_buffer.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/comparisons.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/concatenation.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/conv.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/conv_common.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/depthwise_conv.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/dequantize.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/detection_postprocess.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/elementwise.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/floor.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/fully_connected.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/l2norm.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/logical.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/logistic.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/maximum_minimum.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/mul.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/neg.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/pack.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/pad.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/pooling.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/prelu.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/quantize.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/reduce.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/reshape.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/shape.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/resize_nearest_neighbor.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/round.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/softmax.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/softmax_common.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/split.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/split_v.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/strided_slice.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/sub.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/svdf.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/svdf_common.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/tanh.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/unpack.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/hard_swish.cc"
+set(TFLITE_MICRO_REFERENCE_OPERATOR_SOURCES
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/activations.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/add.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/add_n.cc" 
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/arg_min_max.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/ceil.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/circular_buffer.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/comparisons.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/concatenation.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/conv.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/conv_common.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/cumsum.cc"  
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/depthwise_conv.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/depthwise_conv_common.cc"  
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/dequantize.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/detection_postprocess.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/elementwise.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/elu.cc"  
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/floor.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/fully_connected.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/l2norm.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/logical.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/logistic.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/maximum_minimum.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/mul.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/neg.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/pack.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/pad.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/pooling.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/prelu.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/quantize.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/reduce.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/reshape.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/shape.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/resize_nearest_neighbor.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/round.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/softmax.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/softmax_common.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/split.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/split_v.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/squeeze.cc"  
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/strided_slice.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/sub.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/svdf.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/svdf_common.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/tanh.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/unpack.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/hard_swish.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/floor_div.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/floor_mod.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/l2_pool_2d.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/leaky_relu.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/depth_to_space.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/transpose_conv.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/resize_bilinear.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/batch_to_space_nd.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/space_to_batch_nd.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/transpose.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/pooling_common.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/expand_dims.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/space_to_depth.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/circular_buffer.cc"  
 )
 
 #*************************************************
 # TensorFlow Lite Micro xcore kernel sources
 #*************************************************
-set(TENSORFLOW_LITE_XCORE_OPERATOR_SOURCES
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_activations.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_add.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_arg_min_max.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_avgpool_global.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_bconv2d.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_bsign.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_conv2d.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_custom_options.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_dispatcher.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_fully_connected.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_interpreter.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_memory_loader.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_op_utils.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_pad.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_planning.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_pooling.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_profiler.cc"
-  "${TENSORFLOW_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_type_conversions.cc"
+set(TFLITE_MICRO_XCORE_OPERATOR_SOURCES
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_activations.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_add.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_arg_min_max.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_avgpool_global.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_bconv2d.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_bsign.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_conv2d.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_custom_options.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_dispatcher.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_fully_connected.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_interpreter.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_memory_loader.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_op_utils.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_pad.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_planning.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_pooling.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_profiler.cc"
+  "${TFLITE_MICRO_SOURCE_DIR}/tensorflow/lite/micro/kernels/xcore/xcore_type_conversions.cc"
 )
 
 #**********************
@@ -140,48 +161,44 @@ file(GLOB_RECURSE LIB_NN_SOURCES_C "${LIB_NN_SOURCE_DIR}/lib_nn/src/*.c")
 file(GLOB_RECURSE LIB_NN_SOURCES_ASM "${LIB_NN_SOURCE_DIR}/lib_nn/src/asm/*.S")
 
 #**************************************
-# set XCORE_INTERPRETER user variables
+# set XCORE_RUNTIME user variables
 #**************************************
 
 if (X86)
-  set(XCORE_INTERPRETER_SOURCES
+  set(XCORE_RUNTIME_SOURCES
       ${LIB_NN_SOURCES_C}
-      ${TENSORFLOW_LITE_RUNTIME_SOURCES}
-      ${TENSORFLOW_LITE_REFERENCE_OPERATOR_SOURCES}
-      ${TENSORFLOW_LITE_XCORE_OPERATOR_SOURCES}
+      ${TFLITE_MICRO_RUNTIME_SOURCES}
+      ${TFLITE_MICRO_REFERENCE_OPERATOR_SOURCES}
+      ${TFLITE_MICRO_XCORE_OPERATOR_SOURCES}
   )
 else ()
-  set(XCORE_INTERPRETER_SOURCES
+  set(XCORE_RUNTIME_SOURCES
+    ${TFLITE_MICRO_RUNTIME_SOURCES}
+    ${TFLITE_MICRO_REFERENCE_OPERATOR_SOURCES}
+    ${TFLITE_MICRO_XCORE_OPERATOR_SOURCES}
     ${LIB_NN_SOURCES_C}
-    ${TENSORFLOW_LITE_RUNTIME_SOURCES}
-    ${TENSORFLOW_LITE_REFERENCE_OPERATOR_SOURCES}
-    ${TENSORFLOW_LITE_XCORE_OPERATOR_SOURCES}
     ${LIB_NN_SOURCES_ASM}
   )
 endif ()
 
-set(XCORE_INTERPRETER_INCLUDES
+set(XCORE_RUNTIME_INCLUDES
   ${FLATBUFFERS_INCLUDE_DIR}
   ${GEMMLOWP_INCLUDE_DIR}
   ${RUY_INCLUDE_DIR}
-  ${TENSORFLOW_INCLUDE_DIR}
+  ${TFLITE_MICRO_INCLUDE_DIR}
   ${LIB_NN_ALT_INCLUDE_DIR}
   ${LIB_NN_INCLUDE_DIR}
 )
 
-list(REMOVE_DUPLICATES XCORE_INTERPRETER_SOURCES)
-list(REMOVE_DUPLICATES XCORE_INTERPRETER_INCLUDES)
+list(REMOVE_DUPLICATES XCORE_RUNTIME_SOURCES)
+list(REMOVE_DUPLICATES XCORE_RUNTIME_INCLUDES)
 
 #**************************************
 # set MODEL_RUNNER user variables
 #**************************************
 
 set(MODEL_RUNNER_SOURCES
-    ${TENSORFLOW_LITE_RUNTIME_SOURCES}
-    ${TENSORFLOW_LITE_REFERENCE_OPERATOR_SOURCES}
-    ${TENSORFLOW_LITE_XCORE_OPERATOR_SOURCES}
-    ${LIB_NN_SOURCES_C}
-    ${LIB_NN_SOURCES_ASM}
+    ${XCORE_RUNTIME_SOURCES}
     "${MODEL_RUNNER_DIR}/src/model_runner.cc"
 )
 
@@ -195,12 +212,7 @@ if (DEFINED RTOS_CMAKE_RTOS)
 endif ()
 
 set(MODEL_RUNNER_INCLUDES
-  ${FLATBUFFERS_INCLUDE_DIR}
-  ${GEMMLOWP_INCLUDE_DIR}
-  ${RUY_INCLUDE_DIR}
-  ${TENSORFLOW_INCLUDE_DIR}
-  ${LIB_NN_ALT_INCLUDE_DIR}
-  ${LIB_NN_INCLUDE_DIR}
+  ${XCORE_RUNTIME_INCLUDES}
   "${MODEL_RUNNER_DIR}/api"
 )
 
@@ -213,6 +225,6 @@ list(REMOVE_DUPLICATES MODEL_RUNNER_INCLUDES)
 
 # suppress fptrgroup warnings for now
 if (NOT X86)
-  set_source_files_properties(${XCORE_INTERPRETER_SOURCES} PROPERTIES COMPILE_FLAGS -Wno-xcore-fptrgroup)
+  set_source_files_properties(${XCORE_RUNTIME_SOURCES} PROPERTIES COMPILE_FLAGS -Wno-xcore-fptrgroup)
   set_source_files_properties(${MODEL_RUNNER_SOURCES} PROPERTIES COMPILE_FLAGS -Wno-xcore-fptrgroup)
 endif ()
--- modules/aif/model_runner/src/model_runner.cc
+++ modules/aif/model_runner/src/model_runner.cc
@@ -12,13 +12,14 @@
 #include "tensorflow/lite/micro/kernels/xcore/xcore_interpreter.h"
 #include "tensorflow/lite/micro/micro_error_reporter.h"
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
-#include "tensorflow/lite/version.h"
+#include "tensorflow/lite/micro/micro_op_resolver.h"
 
 #if RTOS_FREERTOS
 #include "rtos_dispatcher.h"
 #endif
 
 // typedefs
+typedef tflite::Model model_t;
 typedef tflite::MicroAllocator micro_allocator_t;
 typedef tflite::SimpleMemoryAllocator simple_allocator_t;
 typedef tflite::MicroErrorReporter error_reporter_t;
@@ -32,31 +33,36 @@ typedef tflite::micro::xcore::Dispatcher tflite_dispatcher_t;
 static error_reporter_t error_reporter_s;
 static memory_loader_t memory_loader_s;
 
+static const model_t *model = nullptr;
 static error_reporter_t *reporter = nullptr;
 static micro_allocator_t *allocator = nullptr;
 static tflite_dispatcher_t *tflite_dispatcher = nullptr;
 
 size_t model_runner_buffer_size_get() { return sizeof(interpreter_t); }
 
-void model_runner_init(uint8_t *arena, size_t arena_size) {
+void model_runner_init(uint8_t *arena, size_t arena_size)
+{
   xassert(arena);
   xassert(arena_size > 0);
 
   // Set up error reporting
-  if (reporter == nullptr) {
+  if (reporter == nullptr)
+  {
     reporter = &error_reporter_s;
   }
 
   // Set up allocator
   static simple_allocator_t simple_allocator_s(reporter, arena, arena_size);
-  if (allocator == nullptr) {
+  if (allocator == nullptr)
+  {
     allocator = micro_allocator_t::Create(&simple_allocator_s, reporter);
   }
 }
 
 #if RTOS_FREERTOS
 ModelRunnerStatus model_runner_dispatcher_create(model_runner_t *ctx,
-                                                 dispatcher_t *dispatcher) {
+                                                 dispatcher_t *dispatcher)
+{
   xassert(dispatcher);
   void *tflite_dispatcher_buf = allocator->AllocatePersistentBuffer(
       sizeof(tflite::micro::xcore::RTOSDispatcher));
@@ -66,7 +72,8 @@ ModelRunnerStatus model_runner_dispatcher_create(model_runner_t *ctx,
   return Ok;
 }
 #else
-ModelRunnerStatus model_runner_dispatcher_create(model_runner_t *ctx) {
+ModelRunnerStatus model_runner_dispatcher_create(model_runner_t *ctx)
+{
   void *tflite_dispatcher_buf = allocator->AllocatePersistentBuffer(
       sizeof(tflite::micro::xcore::GenericDispatcher));
   tflite_dispatcher =
@@ -77,14 +84,15 @@ ModelRunnerStatus model_runner_dispatcher_create(model_runner_t *ctx) {
 #endif
 
 ModelRunnerStatus model_runner_allocate(model_runner_t *ctx,
-                                        const uint8_t *model_content) {
+                                        const uint8_t *model_content)
+{
   xassert(model_content);
 
   // Map the model into a usable data structure. This doesn't involve any
   // copying or parsing, it's a very lightweight operation.
-  const tflite::Model *model;
   model = tflite::GetModel(model_content);
-  if (model->version() != TFLITE_SCHEMA_VERSION) {
+  if (model->version() != TFLITE_SCHEMA_VERSION)
+  {
     return ModelVersionError;
   }
 
@@ -112,7 +120,8 @@ ModelRunnerStatus model_runner_allocate(model_runner_t *ctx,
 #endif
 
   // Allocate buffer for interpreter (if not already allocated)
-  if (ctx->hInterpreter == nullptr) {
+  if (ctx->hInterpreter == nullptr)
+  {
     ctx->hInterpreter = malloc(model_runner_buffer_size_get());
   }
   // Build an interpreter to run the model with
@@ -122,25 +131,29 @@ ModelRunnerStatus model_runner_allocate(model_runner_t *ctx,
 
   // Allocate memory from the tensor_arena for the model's tensors.
   TfLiteStatus allocate_tensors_status = interpreter->AllocateTensors();
-  if (allocate_tensors_status != kTfLiteOk) {
+  if (allocate_tensors_status != kTfLiteOk)
+  {
     return AllocateTensorsError;
   }
 
   return Ok;
 }
 
-int8_t *model_runner_input_buffer_get(model_runner_t *ctx) {
+int8_t *model_runner_input_buffer_get(model_runner_t *ctx)
+{
   interpreter_t *interpreter = static_cast<interpreter_t *>(ctx->hInterpreter);
   return interpreter->input(0)->data.int8;
 }
 
-size_t model_runner_input_size_get(model_runner_t *ctx) {
+size_t model_runner_input_size_get(model_runner_t *ctx)
+{
   interpreter_t *interpreter = static_cast<interpreter_t *>(ctx->hInterpreter);
   return interpreter->input(0)->bytes;
 }
 
 void model_runner_input_quant_get(model_runner_t *ctx, float *scale,
-                                  int *zero_point) {
+                                  int *zero_point)
+{
   xassert(scale);
   xassert(zero_point);
 
@@ -149,7 +162,8 @@ void model_runner_input_quant_get(model_runner_t *ctx, float *scale,
   *zero_point = interpreter->input(0)->params.zero_point;
 }
 
-ModelRunnerStatus model_runner_invoke(model_runner_t *ctx) {
+ModelRunnerStatus model_runner_invoke(model_runner_t *ctx)
+{
   interpreter_t *interpreter = static_cast<interpreter_t *>(ctx->hInterpreter);
 
   // Rset the profiler
@@ -158,25 +172,29 @@ ModelRunnerStatus model_runner_invoke(model_runner_t *ctx) {
   // Run inference, and report any error
   TfLiteStatus invoke_status = interpreter->Invoke();
 
-  if (invoke_status != kTfLiteOk) {
+  if (invoke_status != kTfLiteOk)
+  {
     return InvokeError;
   }
 
   return Ok;
 }
 
-int8_t *model_runner_output_buffer_get(model_runner_t *ctx) {
+int8_t *model_runner_output_buffer_get(model_runner_t *ctx)
+{
   interpreter_t *interpreter = static_cast<interpreter_t *>(ctx->hInterpreter);
   return interpreter->output(0)->data.int8;
 }
 
-size_t model_runner_output_size_get(model_runner_t *ctx) {
+size_t model_runner_output_size_get(model_runner_t *ctx)
+{
   interpreter_t *interpreter = static_cast<interpreter_t *>(ctx->hInterpreter);
   return interpreter->output(0)->bytes;
 }
 
 void model_runner_output_quant_get(model_runner_t *ctx, float *scale,
-                                   int *zero_point) {
+                                   int *zero_point)
+{
   xassert(scale);
   xassert(zero_point);
 
@@ -188,32 +206,50 @@ void model_runner_output_quant_get(model_runner_t *ctx, float *scale,
 #ifndef NDEBUG
 
 void model_runner_profiler_durations_get(model_runner_t *ctx, uint32_t *count,
-                                         const uint32_t **durations) {
+                                         const uint32_t **durations)
+{
   xassert(count);
   xassert(durations);
 
   ctx->profiler_durations_get_fun(count, durations);
 }
 
-void model_runner_profiler_summary_print(model_runner_t *ctx) {
+void model_runner_profiler_summary_print(model_runner_t *ctx)
+{
   uint32_t count = 0;
   uint32_t total = 0;
   uint32_t time_us = 0;
   const uint32_t *durations = nullptr;
+  void *v_resolver = nullptr;
   const char *op_name;
 
-  interpreter_t *interpreter = static_cast<interpreter_t *>(ctx->hInterpreter);
-
   ctx->profiler_durations_get_fun(&count, &durations);
+  ctx->resolver_get_fun(&v_resolver);
 
-  for (size_t i = 0; i < interpreter->operators_size(); ++i) {
-    if (i < count) {
-      tflite::NodeAndRegistration node_and_reg =
-          interpreter->node_and_registration(static_cast<int>(i));
-      const TfLiteRegistration *registration = node_and_reg.registration;
-      if (registration->builtin_code == tflite::BuiltinOperator_CUSTOM) {
+  size_t subgraph_idx = 0;
+  const tflite::SubGraph *subgraph = model->subgraphs()->Get(subgraph_idx);
+  auto *opcodes = model->operator_codes();
+  uint32_t operators_size = NumSubgraphOperators(subgraph);
+  const tflite::OpResolver *c_resolver = static_cast<const tflite::OpResolver *>(v_resolver);
+
+  for (size_t i = 0; i < operators_size; ++i)
+  {
+    if (i < count)
+    {
+      const auto *op = subgraph->operators()->Get(i);
+      const size_t index = op->opcode_index();
+      const auto *opcode = opcodes->Get(index);
+      const TfLiteRegistration *registration = nullptr;
+
+      GetRegistrationFromOpCode(opcode, *c_resolver, reporter,
+                                &registration);
+
+      if (registration->builtin_code == tflite::BuiltinOperator_CUSTOM)
+      {
         op_name = registration->custom_name;
-      } else {
+      }
+      else
+      {
         op_name = tflite::EnumNameBuiltinOperator(
             tflite::BuiltinOperator(registration->builtin_code));
       }
