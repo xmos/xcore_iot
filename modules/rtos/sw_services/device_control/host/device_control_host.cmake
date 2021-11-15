6b79e727bfceced19fa6904b3214de7ec3c51411
John Shafer <johnshafer@xmos.com>
2021-11-15 13:14:57 -0500

    ensure this cmake only uses the sdk in which it resides

--- modules/rtos/sw_services/device_control/host/device_control_host.cmake
+++ modules/rtos/sw_services/device_control/host/device_control_host.cmake
@@ -4,7 +4,15 @@ option(DEVICE_CONTROL_USE_USB "Device control host uses USB" ON)
 option(DEVICE_CONTROL_USE_RPI_I2C "Device control host uses I2C from a Raspberry Pi " OFF)
 option(DEVICE_CONTROL_USE_AARDVARK_I2C "Device control host uses I2C with an Aardvark" OFF)
 
-set(DEVICE_CONTROL_PATH "$ENV{XCORE_SDK_PATH}/modules/rtos/sw_services/device_control/host")
+set(SDK_ROOT_PATH "${CMAKE_CURRENT_LIST_DIR}")
+cmake_path(GET SDK_ROOT_PATH PARENT_PATH SDK_ROOT_PATH)
+cmake_path(GET SDK_ROOT_PATH PARENT_PATH SDK_ROOT_PATH)
+cmake_path(GET SDK_ROOT_PATH PARENT_PATH SDK_ROOT_PATH)
+cmake_path(GET SDK_ROOT_PATH PARENT_PATH SDK_ROOT_PATH)
+cmake_path(GET SDK_ROOT_PATH PARENT_PATH SDK_ROOT_PATH)
+message(STATUS "Using SDK at ${SDK_ROOT_PATH}")
+
+set(DEVICE_CONTROL_PATH "${SDK_ROOT_PATH}/modules/rtos/sw_services/device_control/host")
 
 # Tell cmake where to find the macros/functions for things like finding libusb
 list(APPEND CMAKE_MODULE_PATH ${DEVICE_CONTROL_PATH}/.cmake)
@@ -28,16 +36,16 @@ if (DEVICE_CONTROL_USE_USB)
 
     # Discern OS for libusb library location
     if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-        link_directories("$ENV{XCORE_SDK_PATH}/modules/thirdparty/libusb/OSX64")
-        set(libusb-1.0_INCLUDE_DIRS "$ENV{XCORE_SDK_PATH}/modules/thirdparty/libusb/OSX64")
+        link_directories("${SDK_ROOT_PATH}/modules/thirdparty/libusb/OSX64")
+        set(libusb-1.0_INCLUDE_DIRS "${SDK_ROOT_PATH}/modules/thirdparty/libusb/OSX64")
         set(LINK_LIBS ${LINK_LIBS} usb-1.0.0)
     elseif (${CMAKE_SYSTEM_NAME} MATCHES "Linux")
         find_package(PkgConfig)
         pkg_check_modules(libusb-1.0 REQUIRED libusb-1.0)
         set(LINK_LIBS ${LINK_LIBS} usb-1.0)
     elseif (${CMAKE_SYSTEM_NAME} MATCHES "Windows")
-        link_directories("$ENV{XCORE_SDK_PATH}/modules/thirdparty/libusb/Win32")
-        set(libusb-1.0_INCLUDE_DIRS "$ENV{XCORE_SDK_PATH}/modules/thirdparty/libusb/Win32")
+        link_directories("${SDK_ROOT_PATH}/modules/thirdparty/libusb/Win32")
+        set(libusb-1.0_INCLUDE_DIRS "${SDK_ROOT_PATH}/modules/thirdparty/libusb/Win32")
         set(LINK_LIBS ${LINK_LIBS} libusb)
     endif()
 
