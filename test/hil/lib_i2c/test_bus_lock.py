2d0f16f9274a27249cdde5db923625d1db6b242a
Angel Cascarino <angelcascarino@xmos.com>
2021-11-23 16:13:22 +0000

    Ported test_bus_lock to pytest

--- test/hil/lib_i2c/test_bus_lock.py
+++ test/hil/lib_i2c/test_bus_lock.py
@@ -1,29 +1,29 @@
 # Copyright 2014-2021 XMOS LIMITED.
 # This Software is subject to the terms of the XMOS Public Licence: Version 1.
-import xmostest
+import Pyxsim as px
 from i2c_master_checker import I2CMasterChecker
 
-
-def do_locks_test():
-    resources = xmostest.request_resource("xsim")
-
+def test_bus_lock(build, capfd):
     binary = 'i2c_test_locks/bin/i2c_test_locks.xe' 
 
     checker = I2CMasterChecker("tile[0]:XS1_PORT_1A",
                                "tile[0]:XS1_PORT_1B",
                                expected_speed = 400)
 
-    tester = xmostest.ComparisonTester(open('expected/lock_test.expect'),
-                                     'lib_i2c', 'i2c_master_sim_tests',
-                                     'bus_locks',
-                                     {},
-                                     regexp=True)
+    tester = px.testers.PytestComparisonTester('expected/lock_test.expect',
+                                                regexp = True,
+                                                ordered = True)
 
-    xmostest.run_on_simulator(resources['xsim'], binary,
-                              simthreads = [checker],
-                              simargs=['--weak-external-drive'],
-                              suppress_multidrive_messages=True,
-                              tester = tester)
+    sim_args = ['--weak-external-drive']
 
-def runtest():
-    do_locks_test()
+    build(binary)
+
+    px.run_with_pyxsim(binary,
+                    simthreads = [checker],
+                    simargs = sim_args)
+
+    # The first two lines of this test are not reflected in the expectation file
+    # and vary based on the test; cut them out.
+    outcapture = capfd.readouterr().out.split("\n")[2:]
+
+    tester.run(outcapture)
