a9a38f678cba393a3010f405535b229a4b64bd55
John Shafer <johnshafer@xmos.com>
2021-12-16 13:52:26 -0500

    fatfs_mkimage host app Windows installation location change unique to XCore SDK version.

--- modules/rtos/sw_services/fatfs/host/install_fatfs_util.bat
+++ modules/rtos/sw_services/fatfs/host/install_fatfs_util.bat
@@ -4,32 +4,48 @@
 
 @echo off
 
+setlocal enabledelayedexpansion
 
-:: run CMake for fresh executables
+
+:: build program for fresh executables
 rm -rf build
 cmake -B build -G "NMake Makefiles"
 cd build
 nmake
 
-:: check if .xutils\ directory exists
-if exist "%USERPROFILE%\.xutils\" (
+:: attain version of XCore SDK
+:: read XCore SDK file "settings.json", removing line breaks
+set jsonString=
+for /f "delims=" %%x in (%XCORE_SDK_PATH%\settings.json) do set "jsonString=!jsonString!%%x"
+
+:: remove braces
+set "jsonString=%jsonString:~2,-2%"
+:: remove quotes
+set jsonString=%jsonString:"=%
+:: change colon to equals
+set "jsonString=%jsonString:: ==%"
+:: parse into keys and values
+set "%jsonString:, =" & set "%"
+
+:: check if .xmos\SDK\%version%\bin directory exists
+if exist "%USERPROFILE%\.xmos\SDK\%version%\bin" (
     echo.
     echo overwriting existing executables
 ) else (
-    mkdir "%USERPROFILE%\.xutils\"
+    mkdir "%USERPROFILE%\.xmos\SDK\%version%\bin"
 )
 
-:: move fatfs_mkimage executables to %USERPROFILE%\.xutils
+:: move fatfs_mkimage executables to %USERPROFILE%\.xmos\SDK\%version%\bin
 cd ..
-mv fat*.* "%USERPROFILE%"\.xutils\
+mv fat*.* "%USERPROFILE%"\.xmos\SDK\%version%\bin
 
 :: create a convenient, session-only path environment variable to executables
-set PATH=%PATH%;%USERPROFILE%\.xutils
+set PATH=%PATH%;%USERPROFILE%\.xmos\SDK\%version%\bin
 
 :: alert user where it is, and that it has been added to temporary env vars
 echo.
-echo Utility executables for fatfs_mkimage created at "%USERPROFILE%\.xutils\"
-echo and PATH environment variable set for this instance only; add to environment
-echo variables for permanence.
+echo.
+echo Utility executables for fatfs_mkimage created at "%USERPROFILE%\.xmos\SDK\%version%\bin\".
+echo Please add to the PATH system environment variable.
 echo.
 echo Test with fatfs_mkimage.exe --help
