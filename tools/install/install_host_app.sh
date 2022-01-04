c4f66bc3e2e1e9b4c9024e372433a364623368cd
johnshaferXMOS <johnshafer@xmos.com>
2022-01-04 17:06:35 -0500

    added linux script for host app, need to debug.

---
+++ tools/install/install_host_app.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ -z ${XCORE_SDK_PATH} ]; then
+    echo "XCORE_SDK_PATH must be set before running this script."
+else
+    # build program for fresh executables
+    INSTALL_SCRIPT_PATH=$PWD
+    HOSTAPP_PATH=${XCORE_SDK_PATH}/modules/rtos/sw_services/fatfs/host
+    
+    cd ${HOSTAPP_PATH}
+    rm -rf bin
+    rm -rf build
+    cmake -B build
+    (cd build ; make -j)
+    cd ${INSTALL_SCRIPT_PATH}
+
+    # alert user where it is
+    echo
+    echo
+    echo "Utility executables for fatfs_mkimage created at /opt/xmos/SDK/version/bin/."
+    echo "Please add to the PATH system environment variable."
+    echo
+    echo "Then, test with fatfs_mkimage.exe --help"
+fi
