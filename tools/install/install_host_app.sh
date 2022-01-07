948a6e25586d1ddb4bd3a7174377f164965bf556
johnshaferXMOS <johnshafer@xmos.com>
2022-01-07 13:44:55 -0500

    Linux script installs with sudo on machines where user is not the owner of /opt/

--- tools/install/install_host_app.sh
+++ tools/install/install_host_app.sh
@@ -1,26 +1,23 @@
 #!/bin/sh
 
-if [ -z ${XCORE_SDK_PATH} ]; then
-    echo "XCORE_SDK_PATH must be set before running this script."
-else
-    # build program for fresh executables
-    INSTALL_SCRIPT_PATH=$PWD
-    HOSTAPP_PATH=${XCORE_SDK_PATH}/modules/rtos/sw_services/fatfs/host
-    
-    echo $INSTALL_SCRIPT_PATH
-    echo $HOSTAPP_PATH
-    cd ${HOSTAPP_PATH}
-    rm -rf bin
-    rm -rf build
-    cmake -B build
-    (cd build ; make -j)
-    cd ${INSTALL_SCRIPT_PATH}
+# build program for fresh executables
+INSTALL_SCRIPT_PATH=$PWD
+XCORE_SDK_REPO_PATH=$(git rev-parse --show-toplevel)
+HOSTAPP_PATH=$XCORE_SDK_REPO_PATH/modules/rtos/sw_services/fatfs/host
 
-    # alert user where it is
-    echo
-    echo
-    echo "Utility executables for fatfs_mkimage created at /opt/xmos/SDK/version/bin/."
-    echo "Please add to the PATH system environment variable."
-    echo
-    echo "Then, test with fatfs_mkimage.exe --help"
-fi
+echo $INSTALL_SCRIPT_PATH
+echo $HOSTAPP_PATH
+cd ${HOSTAPP_PATH}
+rm -rf bin
+rm -rf build
+cmake -B build
+(cd build ; make -j)
+cd ${INSTALL_SCRIPT_PATH}
+
+# alert user where it is
+echo
+echo
+echo "Utility executables for fatfs_mkimage created at /opt/xmos/SDK/version/bin/."
+echo "Please add to the PATH system environment variable."
+echo
+echo "Then, test with fatfs_mkimage.exe --help"
