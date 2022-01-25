9a5e5f2c5bdb31725011ccc856980f33fb942532
johnshaferXMOS <johnshafer@xmos.com>
2022-01-25 12:50:19 -0500

    clean code of host app script

--- tools/install/install_host_apps.sh
+++ tools/install/install_host_apps.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 
-# build program for fresh executables
-INSTALL_SCRIPT_PATH=$PWD
-XCORE_SDK_REPO_PATH=$(git rev-parse --show-toplevel)
-HOSTAPP_PATH=$XCORE_SDK_REPO_PATH/modules/rtos/sw_services/fatfs/host
+# Build program for fresh executables
+# Get paths
+PATH_INSTALL_SCRIPT=$PWD
+PATH_XCORE_SDK_REPO=$(git rev-parse --show-toplevel)
+PATH_HOSTAPP=$PATH_XCORE_SDK_REPO/modules/rtos/sw_services/fatfs/host
 
-echo $INSTALL_SCRIPT_PATH
-echo $HOSTAPP_PATH
-cd ${HOSTAPP_PATH}
+cd ${PATH_HOSTAPP}
 rm -rf bin
 rm -rf build
 cmake -B build
 (cd build ; make -j)
-cd ${INSTALL_SCRIPT_PATH}
+cd ${PATH_INSTALL_SCRIPT}
 
 # alert user where it is
 echo
 echo
-echo "Utility executables created at /opt/xmos/SDK/version/bin/."
+echo "Utility executables created at /opt/xmos/SDK/version/bin/"
 echo "Please add to the PATH system environment variable."
 echo
 echo "Then, test with: fatfs_mkimage --help"
