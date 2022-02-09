c17a9276e6a1acdc93b5b9774fba7d4f003e59cc
Jeremiah McCarthy <jeremiahmccarthy@xmos.com>
2022-02-09 17:16:38 -0500

    Cleanup and add run and debug targets

--- tools/cmake_utils/create_multitile_target.cmake
+++ tools/cmake_utils/create_multitile_target.cmake
@@ -7,25 +7,53 @@
 ## contains the _BASE_TARGET application with tile _TILE_TO_MERGE replaced with
 ## the respective tile from _OTHER_TARGET.
 macro(merge_binaries _OUTPUT_TARGET_NAME _BASE_TARGET _OTHER_TARGET _TILE_NUM_TO_MERGE)
-    get_target_property(BASE_TILE_DIR ${_BASE_TARGET} BINARY_DIR)
-    get_target_property(BASE_TILE_NAME ${_BASE_TARGET} NAME)
-    get_target_property(OTHER_TILE_DIR ${_OTHER_TARGET} BINARY_DIR)
-    get_target_property(OTHER_TILE_NAME ${_OTHER_TARGET} NAME)
+    get_target_property(BASE_TILE_DIR     ${_BASE_TARGET}  BINARY_DIR)
+    get_target_property(BASE_TILE_NAME    ${_BASE_TARGET}  NAME)
+    get_target_property(OTHER_TILE_NAME   ${_OTHER_TARGET} NAME)
 
-    file(MAKE_DIRECTORY ${OTHER_TILE_DIR}/${OTHER_TILE_NAME}_split)
+    file(MAKE_DIRECTORY ${OTHER_TILE_NAME}_split)
     add_custom_target(${_OUTPUT_TARGET_NAME}
-        ALL
-        COMMAND xobjdump --split --split-dir ${OTHER_TILE_DIR}/${OTHER_TILE_NAME}_split ${OTHER_TILE_DIR}/${OTHER_TILE_NAME}
-        COMMAND xobjdump ${BASE_TILE_DIR}/${BASE_TILE_NAME} -r 0,1,${OTHER_TILE_DIR}/${OTHER_TILE_NAME}_split/image_n0c${_TILE_NUM_TO_MERGE}_2.elf
-        COMMAND cp ${BASE_TILE_DIR}/${BASE_TILE_NAME} ${BASE_TILE_DIR}/${_OUTPUT_TARGET_NAME}
+        COMMAND xobjdump --split --split-dir ${OTHER_TILE_NAME}_split ${OTHER_TILE_NAME}.xe
+        COMMAND xobjdump ${BASE_TILE_NAME}.xe -r 0,${_TILE_NUM_TO_MERGE},${OTHER_TILE_NAME}_split/image_n0c${_TILE_NUM_TO_MERGE}_2.elf
+        COMMAND cp ${BASE_TILE_NAME}.xe ${_OUTPUT_TARGET_NAME}.xe
         DEPENDS
             ${_BASE_TARGET}
             ${_OTHER_TARGET}
         BYPRODUCTS
-            ${OTHER_TILE_DIR}/${OTHER_TILE_NAME}_split
+            ${OTHER_TILE_NAME}_split/config.xml
+            ${OTHER_TILE_NAME}_split/image_n0c0.elf
+            ${OTHER_TILE_NAME}_split/image_n0c1.elf
+            ${OTHER_TILE_NAME}_split/program_info.txt
+            ${OTHER_TILE_NAME}_split/image_n0c0_2.elf
+            ${OTHER_TILE_NAME}_split/image_n0c1_2.elf
+            ${OTHER_TILE_NAME}_split/platform_def.xn
+            ${OTHER_TILE_NAME}_split/xscope.xscope
         WORKING_DIRECTORY
+            ${BASE_TILE_DIR}
         COMMENT
-            "Merging tile ${_TILE_NUM_TO_MERGE} of ${_OTHER_TARGET} into ${_BASE_TARGET} to create ${_OUTPUT_TARGET_NAME}"
+            "Merging tile ${_TILE_NUM_TO_MERGE} of ${_OTHER_TARGET}.xe into ${_BASE_TARGET}.xe to create ${_OUTPUT_TARGET_NAME}.xe"
         VERBATIM
+    )
+    set_target_properties(${_OUTPUT_TARGET_NAME} PROPERTIES BINARY_DIR ${BASE_TILE_DIR})
+endmacro()
+
+## Creates a run target for a provided binary
+macro(create_run_target _EXECUTABLE_TARGET_NAME)
+    add_custom_target(run_${_EXECUTABLE_TARGET_NAME}
+      COMMAND xrun --xscope ${_EXECUTABLE_TARGET_NAME}.xe
+      DEPENDS ${_EXECUTABLE_TARGET_NAME}
+      COMMENT
+        "Run application"
+      VERBATIM
+    )
+endmacro()
+
+## Creates a debug target for a provided binary
+macro(create_debug_target _EXECUTABLE_NAME)
+    add_custom_target(debug_${_EXECUTABLE_NAME}
+      COMMAND xgdb ${_EXECUTABLE_NAME}.xe -ex 'connect' -ex 'connect --xscope' -ex 'run'
+      DEPENDS ${_EXECUTABLE_NAME}.xe
+      COMMENT
+        "Debug application"
     )
 endmacro()
