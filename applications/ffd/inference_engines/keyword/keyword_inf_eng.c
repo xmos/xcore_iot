d74f825db864c2480b3b9bde96b7f3d678240bca
Keith Moulton <keithm@xmos.com>
2022-03-15 16:22:12 -0400

    features appear to be computer ok

--- applications/ffd/inference_engines/keyword/keyword_inf_eng.c
+++ applications/ffd/inference_engines/keyword/keyword_inf_eng.c
@@ -20,6 +20,12 @@
 #include "tensorflow/lite/experimental/microfrontend/lib/frontend.h"
 #include "tensorflow/lite/experimental/microfrontend/lib/frontend_util.h"
 
+#define NUM_FRAMES_PER_INFERENCE (2)
+#define AUDIO_BUFFER_LENGTH                                                    \
+  (appconfINFERENCE_FRAMES_PER_INFERENCE * NUM_FRAMES_PER_INFERENCE)
+#define AUDIO_BUFFER_STRIDE_LENGTH                                             \
+  (AUDIO_BUFFER_LENGTH - appconfINFERENCE_FRAMES_PER_INFERENCE)
+
 void keyword_engine_task(void *args) {
   StreamBufferHandle_t input_queue = (StreamBufferHandle_t)args;
 
@@ -27,13 +33,14 @@ void keyword_engine_task(void *args) {
   struct FrontendState frontend_state;
   struct FrontendOutput frontend_output;
   int32_t buf[appconfINFERENCE_FRAMES_PER_INFERENCE];
-  int16_t audio16[appconfINFERENCE_FRAMES_PER_INFERENCE * 2];
-  size_t num_samples_received = 0;
+  int16_t audio16[AUDIO_BUFFER_LENGTH];
+  size_t audio16_index = 0;
+  size_t num_frontend_samples_read;
 
   /* Perform any initialization here */
   FrontendFillConfigWithDefaults(&frontend_config);
   frontend_config.window.size_ms = 30;
-  frontend_config.window.step_size_ms = 15;
+  frontend_config.window.step_size_ms = 30;
   frontend_config.noise_reduction.smoothing_bits = 10;
   frontend_config.filterbank.num_channels = 16;
   frontend_config.filterbank.lower_band_limit = 8.0;
@@ -45,13 +52,13 @@ void keyword_engine_task(void *args) {
   frontend_config.pcan_gain_control.enable_pcan = false;
   frontend_config.log_scale.enable_log = true;
   frontend_config.log_scale.scale_shift = 6;
-  FrontendPopulateState(&frontend_config, &frontend_state, 16000);
+  FrontendPopulateState(&frontend_config, &frontend_state,
+                        appconfAUDIO_PIPELINE_SAMPLE_RATE);
 
-  //TODO: Sync frontend_config settings with the model training
-  //TODO: need 2 buffers, and overlap
-  //TODO:  store input tensor of features [65*16]
-  //            Need to "quantize" the features
-  //TODO: call inference every TBD audio frames
+  // TODO: Sync frontend_config settings with the model training
+  // TODO:  store input tensor of features [65*16]
+  //             Need to "quantize" the features
+  // TODO: call inference every TBD audio frames
 
   while (1) {
     /* Receive audio frames */
@@ -65,30 +72,29 @@ void keyword_engine_task(void *args) {
       buf_ptr += bytes_rxed;
     } while (buf_len > 0);
 
-    /* Audio is int32, convert to int16 */
-    num_samples_received += appconfINFERENCE_FRAMES_PER_INFERENCE;
     for (int i = 0; i < appconfINFERENCE_FRAMES_PER_INFERENCE; ++i) {
-      audio16[i] = buf[i] >> 16;
+      /* Audio is int32, convert to int16 */
+      audio16[audio16_index] = (int16_t)(buf[i] >> 16);
+      if (audio16_index++ == AUDIO_BUFFER_LENGTH) {
+        /* Compute features */
+        frontend_output = FrontendProcessSamples(&frontend_state, audio16,
+                                                 AUDIO_BUFFER_LENGTH,
+                                                 &num_frontend_samples_read);
+
+        configASSERT(num_frontend_samples_read == AUDIO_BUFFER_LENGTH);
+        for (int i = 0; i < frontend_output.size; ++i) {
+          printf("%d ",
+                 (int)frontend_output.values[i]); // Print the feature
+        }
+
+        /* Shift the audio buffer left one stride*/
+        memcpy(&audio16[0], &audio16[appconfINFERENCE_FRAMES_PER_INFERENCE],
+               AUDIO_BUFFER_STRIDE_LENGTH * sizeof(audio16[0]));
+        audio16_index = AUDIO_BUFFER_STRIDE_LENGTH;
+      }
     }
-
-
-    rtos_printf("appconfINFERENCE_FRAMES_PER_INFERENCE=%d\n",
-                appconfINFERENCE_FRAMES_PER_INFERENCE);
-
-    /* Compute features */
-    size_t num_samples_read;
-    frontend_output = FrontendProcessSamples(
-        &frontend_state, buf16,
-        sizeof(appconfINFERENCE_FRAMES_PER_INFERENCE) / sizeof(int16_t),
-        &num_samples_read);
-
-    rtos_printf("num_samples_read=%d\n", num_samples_read);
-    for (int i = 0; i < frontend_output.size; ++i) {
-      printf("%d ", (int)frontend_output.values[i]); // Print the feature vector.
-    }
-    rtos_printf("\n");
 
     /* Perform inference here */
-    rtos_printf("inference\n");
+    // rtos_printf("inference\n");
   }
 }
