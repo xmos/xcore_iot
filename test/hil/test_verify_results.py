0113e27d4db11a4da2c1382b251125c14a848f88
keithm-xmos <35048036+keithm-xmos@users.noreply.github.com>
2022-04-29 12:19:26 -0400

    Adding HIL unit tests (#395)
    * Now running HIL tests

--- test/hil/test_verify_results.py
+++ test/hil/test_verify_results.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 # Copyright 2021 XMOS LIMITED.
 # This Software is subject to the terms of the XMOS Public Licence: Version 1.
+import sys
 import pytest
 import xml.etree.ElementTree as ET
 
-libs = ("lib_i2c", "lib_i2s", "lib_spi")
+if len(sys.argv) == 2:
+    libs = ["lib_i2c", "lib_i2s", "lib_spi"]
+else:
+    libs = sys.argv[2:]
+
 acceptable_outcomes = ("skipped",)
 result_fname = "test_results.xml"
 
-test_libs = {lib:lib + "/" + result_fname for lib in libs}
+test_libs = {lib: lib + "/" + result_fname for lib in libs}
+
 
 @pytest.mark.parametrize("fname", test_libs.values(), ids=test_libs.keys())
 def test_results(fname):
@@ -23,10 +29,11 @@ def test_results(fname):
         # Test passed if there are no children
         if len(testcase) == 0:
             continue
-        # Otherwise, test was either skipped, errored, or failed. The testcase 
+        # Otherwise, test was either skipped, errored, or failed. The testcase
         # will have a child with a relevant tag - skipped, error, or failure.
         # If the tag is acceptable then carry on, or otherwise assert failure.
         else:
             for child in testcase:
-                assert child.tag in acceptable_outcomes, \
-                    f"A test reports as {child.tag}, which is not accepted."
+                assert (
+                    child.tag in acceptable_outcomes
+                ), f"A test reports as {child.tag}, which is not accepted."
