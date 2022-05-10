b18d8fcb606ec7a0f31a4520d8a6352095bf7dd8
Jeremiah McCarthy <jeremiahmccarthy@xmos.com>
2022-05-10 16:52:13 -0400

    Update tinyusb submodule

--- modules/rtos/sw_services/usb/portable/dcd_xcore.c
+++ modules/rtos/sw_services/usb/portable/dcd_xcore.c
@@ -381,6 +381,12 @@ bool dcd_edpt_open(uint8_t rhport,
     return true;
 }
 
+void dcd_edpt_close_all (uint8_t rhport)
+{
+    (void) rhport;
+    // TODO see dcd_edpt_close()
+}
+
 void dcd_edpt_close(uint8_t rhport,
                     uint8_t ep_addr)
 {
