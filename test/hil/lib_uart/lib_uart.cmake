1cc0afe951eabdda536889811614fa9aa2021fcc
Jeremiah McCarthy <jeremiahmccarthy@xmos.com>
2022-05-23 16:26:08 -0400

    Cleanup tests

--- test/hil/lib_uart/lib_uart.cmake
+++
@@ -1,4 +0,0 @@
-include(${CMAKE_CURRENT_LIST_DIR}/uart_test_tx/uart_test_tx.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/uart_test_rx/uart_test_rx.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/uart_test_fifo/uart_test_fifo.cmake)
-# include(${CMAKE_CURRENT_LIST_DIR}/uart_test_loopback/uart_test_loopback.cmake)
