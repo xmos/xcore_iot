a72197aa42a60570778fa693859a12c2c9475920
keithm-xmos <35048036+keithm-xmos@users.noreply.github.com>
2022-06-15 15:36:01 -0400

    attempt to install fatfs_mkimage

--- tools/ci/build_rtos_examples.sh
+++ tools/ci/build_rtos_examples.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# add dist_host to path.
+#   This is used in CI for fatfs_mkimage
+PATH=$PATH:dist_host
+
 XCORE_SDK_ROOT=`git rev-parse --show-toplevel`
 
 source ${XCORE_SDK_ROOT}/tools/ci/helper_functions.sh
@@ -10,15 +14,21 @@ DIST_DIR=${XCORE_SDK_ROOT}/dist
 mkdir -p ${DIST_DIR}
 
 # row format is: "name app_target run_fs_target BOARD toolchain"
+ls dist_host/*
 applications=(
-    "cifar10          example_freertos_cifar10          No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
-    "device_control   example_freertos_device_control   No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
-    "dispatcher       example_freertos_dispatcher       No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
     "explorer_board   example_freertos_explorer_board   Yes XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
     "getting_started  example_freertos_getting_started  No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
-    "iot              example_freertos_iot              No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
-    "l2_cache         example_freertos_l2_cache         No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
 )
+
+# applications=(
+#     "cifar10          example_freertos_cifar10          No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+#     "device_control   example_freertos_device_control   No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+#     "dispatcher       example_freertos_dispatcher       No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+#     "explorer_board   example_freertos_explorer_board   Yes XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+#     "getting_started  example_freertos_getting_started  No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+#     "iot              example_freertos_iot              No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+#     "l2_cache         example_freertos_l2_cache         No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+# )
 
 # perform builds
 for ((i = 0; i < ${#applications[@]}; i += 1)); do
