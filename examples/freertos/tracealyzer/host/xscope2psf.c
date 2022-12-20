bf6261e9906b7c4c2027cfd866e44ea5f6715c8f
keithm-xmos <35048036+keithm-xmos@users.noreply.github.com>
2022-12-20 14:48:11 -0500

    fixed xscope2psf build

--- examples/freertos/tracealyzer/host/xscope2psf.c
+++ examples/freertos/tracealyzer/host/xscope2psf.c
@@ -2,6 +2,7 @@
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <stdint.h>
