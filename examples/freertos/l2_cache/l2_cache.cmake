fbe3c2be06c7053b24fde797cafa8b135381b8f8
Jon Carrier <jonathancarrier@xmos.com>
2023-01-03 09:13:51 -0500

    Suppressed stdout of example_freertos_l2_cache's xobjdump
    This output is captured in a log file in case there is a need to
    inspect the output. Errors/warnings are not suppressed.

--- examples/freertos/l2_cache/l2_cache.cmake
+++ examples/freertos/l2_cache/l2_cache.cmake
@@ -65,8 +65,8 @@ create_install_target(example_freertos_l2_cache)
 add_custom_command(
     TARGET example_freertos_l2_cache POST_BUILD
     COMMAND ${CMAKE_COMMAND} -E make_directory example_freertos_l2_cache_split
-    COMMAND xobjdump --strip example_freertos_l2_cache.xe
-    COMMAND xobjdump --split --split-dir example_freertos_l2_cache_split example_freertos_l2_cache.xb
+    COMMAND xobjdump --strip example_freertos_l2_cache.xe > example_freertos_l2_cache_split/output.log
+    COMMAND xobjdump --split --split-dir example_freertos_l2_cache_split example_freertos_l2_cache.xb >> example_freertos_l2_cache_split/output.log
     BYPRODUCTS
         example_freertos_l2_cache.xb
         example_freertos_l2_cache_split
