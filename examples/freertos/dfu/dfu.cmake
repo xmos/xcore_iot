c294507827c77d9d435a1d12629a58f75ce72031
Jeremiah McCarthy <jeremiahmccarthy@xmos.com>
2023-05-02 10:34:12 -0400

    Extend DFU app with loader

--- examples/freertos/dfu/dfu.cmake
+++ examples/freertos/dfu/dfu.cmake
@@ -44,76 +44,60 @@ set(APP_LINK_LIBRARIES
 #**********************
 # Tile Targets
 #**********************
-set(TARGET_NAME tile0_example_freertos_dfu_v1)
-add_executable(${TARGET_NAME} EXCLUDE_FROM_ALL)
-target_sources(${TARGET_NAME} PUBLIC ${APP_SOURCES})
-target_include_directories(${TARGET_NAME} PUBLIC ${APP_INCLUDES})
-target_compile_definitions(${TARGET_NAME} PUBLIC ${APP_COMPILE_DEFINITIONS} THIS_XCORE_TILE=0 VERSION=1)
-target_compile_options(${TARGET_NAME} PRIVATE ${APP_COMPILER_FLAGS})
-target_link_libraries(${TARGET_NAME} PUBLIC ${APP_LINK_LIBRARIES})
-target_link_options(${TARGET_NAME} PRIVATE ${APP_LINK_OPTIONS})
-unset(TARGET_NAME)
+set(VERSIONS 1 2 3)
 
-set(TARGET_NAME tile1_example_freertos_dfu_v1)
-add_executable(${TARGET_NAME} EXCLUDE_FROM_ALL)
-target_sources(${TARGET_NAME} PUBLIC ${APP_SOURCES})
-target_include_directories(${TARGET_NAME} PUBLIC ${APP_INCLUDES})
-target_compile_definitions(${TARGET_NAME} PUBLIC ${APP_COMPILE_DEFINITIONS} THIS_XCORE_TILE=1 VERSION=1)
-target_compile_options(${TARGET_NAME} PRIVATE ${APP_COMPILER_FLAGS})
-target_link_libraries(${TARGET_NAME} PUBLIC ${APP_LINK_LIBRARIES})
-target_link_options(${TARGET_NAME} PRIVATE ${APP_LINK_OPTIONS})
-unset(TARGET_NAME)
+foreach(VERSION ${VERSIONS})
+    set(TARGET_NAME tile0_example_freertos_dfu_v${VERSION})
+    add_executable(${TARGET_NAME} EXCLUDE_FROM_ALL)
+    target_sources(${TARGET_NAME} PUBLIC ${APP_SOURCES})
+    target_include_directories(${TARGET_NAME} PUBLIC ${APP_INCLUDES})
+    target_compile_definitions(${TARGET_NAME} PUBLIC ${APP_COMPILE_DEFINITIONS} THIS_XCORE_TILE=0 VERSION=${VERSION})
+    target_compile_options(${TARGET_NAME} PRIVATE ${APP_COMPILER_FLAGS})
+    target_link_libraries(${TARGET_NAME} PUBLIC ${APP_LINK_LIBRARIES})
+    target_link_options(${TARGET_NAME} PRIVATE ${APP_LINK_OPTIONS})
+    unset(TARGET_NAME)
 
-set(TARGET_NAME tile0_example_freertos_dfu_v2)
-add_executable(${TARGET_NAME} EXCLUDE_FROM_ALL)
-target_sources(${TARGET_NAME} PUBLIC ${APP_SOURCES})
-target_include_directories(${TARGET_NAME} PUBLIC ${APP_INCLUDES})
-target_compile_definitions(${TARGET_NAME} PUBLIC ${APP_COMPILE_DEFINITIONS} THIS_XCORE_TILE=0 VERSION=2)
-target_compile_options(${TARGET_NAME} PRIVATE ${APP_COMPILER_FLAGS})
-target_link_libraries(${TARGET_NAME} PUBLIC ${APP_LINK_LIBRARIES})
-target_link_options(${TARGET_NAME} PRIVATE ${APP_LINK_OPTIONS})
-unset(TARGET_NAME)
+    set(TARGET_NAME tile1_example_freertos_dfu_v${VERSION})
+    add_executable(${TARGET_NAME} EXCLUDE_FROM_ALL)
+    target_sources(${TARGET_NAME} PUBLIC ${APP_SOURCES})
+    target_include_directories(${TARGET_NAME} PUBLIC ${APP_INCLUDES})
+    target_compile_definitions(${TARGET_NAME} PUBLIC ${APP_COMPILE_DEFINITIONS} THIS_XCORE_TILE=1 VERSION=${VERSION})
+    target_compile_options(${TARGET_NAME} PRIVATE ${APP_COMPILER_FLAGS})
+    target_link_libraries(${TARGET_NAME} PUBLIC ${APP_LINK_LIBRARIES})
+    target_link_options(${TARGET_NAME} PRIVATE ${APP_LINK_OPTIONS})
+    unset(TARGET_NAME)
 
-set(TARGET_NAME tile1_example_freertos_dfu_v2)
-add_executable(${TARGET_NAME} EXCLUDE_FROM_ALL)
-target_sources(${TARGET_NAME} PUBLIC ${APP_SOURCES})
-target_include_directories(${TARGET_NAME} PUBLIC ${APP_INCLUDES})
-target_compile_definitions(${TARGET_NAME} PUBLIC ${APP_COMPILE_DEFINITIONS} THIS_XCORE_TILE=1 VERSION=2)
-target_compile_options(${TARGET_NAME} PRIVATE ${APP_COMPILER_FLAGS})
-target_link_libraries(${TARGET_NAME} PUBLIC ${APP_LINK_LIBRARIES})
-target_link_options(${TARGET_NAME} PRIVATE ${APP_LINK_OPTIONS})
-unset(TARGET_NAME)
+    #**********************
+    # Merge binaries
+    #**********************
+    merge_binaries(example_freertos_dfu_v${VERSION} tile0_example_freertos_dfu_v${VERSION} tile1_example_freertos_dfu_v${VERSION} 1)
 
-#**********************
-# Merge binaries
-#**********************
-merge_binaries(example_freertos_dfu_v1 tile0_example_freertos_dfu_v1 tile1_example_freertos_dfu_v1 1)
-merge_binaries(example_freertos_dfu_v2 tile0_example_freertos_dfu_v2 tile1_example_freertos_dfu_v2 1)
+    #**********************
+    # Create run and debug targets
+    #**********************
+    create_run_target(example_freertos_dfu_v${VERSION})
+    create_debug_target(example_freertos_dfu_v${VERSION})
+    create_flash_app_target(
+        #[[ Target ]]                 example_freertos_dfu_v${VERSION}
+        #[[ Boot Partition Size ]]    0x100000 
+        #[[ Data Parition Contents ]] 
+        #[[ Dependencies ]]           
+    )
+    query_tools_version()
+    create_upgrade_img_target(example_freertos_dfu_v${VERSION} ${XTC_VERSION_MAJOR} ${XTC_VERSION_MINOR})
+    create_install_target(example_freertos_dfu_v${VERSION})
+    create_erase_all_target(example_freertos_dfu_v${VERSION} ${CMAKE_CURRENT_LIST_DIR}/XCORE-AI-EXPLORER.xn)
 
-#**********************
-# Create run and debug targets
-#**********************
-create_run_target(example_freertos_dfu_v1)
-create_debug_target(example_freertos_dfu_v1)
-create_flash_app_target(
-    #[[ Target ]]                 example_freertos_dfu_v1
-    #[[ Boot Partition Size ]]    0x100000 
-    #[[ Data Parition Contents ]] 
-    #[[ Dependencies ]]           
+    #**********************
+    # Create loader
+    #**********************
+    create_loader_target(example_freertos_dfu_v${VERSION} ${CMAKE_CURRENT_LIST_DIR}/loader/loader.c)
+endforeach()
+
+add_custom_target(example_freertos_dfu_loader_flash
+    COMMAND xflash --quad-spi-clock 50MHz --factory example_freertos_dfu_v1.xe --upgrade 1 example_freertos_dfu_v2 --loader example_freertos_dfu_v1_loader.o
+    DEPENDS
+        example_freertos_dfu_v1_loader
+        example_freertos_dfu_v1
+        example_freertos_dfu_v2
 )
-query_tools_version()
-create_upgrade_img_target(example_freertos_dfu_v1 ${XTC_VERSION_MAJOR} ${XTC_VERSION_MINOR})
-create_install_target(example_freertos_dfu_v1)
-create_erase_all_target(example_freertos_dfu_v1 ${CMAKE_CURRENT_LIST_DIR}/XCORE-AI-EXPLORER.xn)
-
-create_run_target(example_freertos_dfu_v2)
-create_debug_target(example_freertos_dfu_v2)
-create_flash_app_target(
-    #[[ Target ]]                 example_freertos_dfu_v2
-    #[[ Boot Partition Size ]]    0x100000 
-    #[[ Data Parition Contents ]] 
-    #[[ Dependencies ]]           
-)
-create_upgrade_img_target(example_freertos_dfu_v2 ${XTC_VERSION_MAJOR} ${XTC_VERSION_MINOR})
-create_install_target(example_freertos_dfu_v2)
-create_erase_all_target(example_freertos_dfu_v2 ${CMAKE_CURRENT_LIST_DIR}/XCORE-AI-EXPLORER.xn)
--- tools/ci/build_rtos_core_examples.sh
+++ tools/ci/build_rtos_core_examples.sh
@@ -16,21 +16,23 @@ if [ -d "${DIST_HOST_DIR}" ]; then
     find ${DIST_HOST_DIR} -type f -exec chmod a+x {} +
 fi
 
-# row format is: "target min_tools_version run_fs_target run_swmem_target run_datapartition_target run_upgrade_img_target board toolchain"
+# row format is: "target min_tools_version run_fs_target run_swmem_target run_datapartition_target run_upgrade_img_target run_loader_target board toolchain"
 applications=(
-    "example_freertos_audio_mux        15.1.0  No  No  No  No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
-    "example_freertos_device_control   15.1.0  No  No  No  No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
-    "example_freertos_dfu_v1           15.1.3  No  No  No  Yes XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
-    "example_freertos_dfu_v2           15.1.3  No  No  No  Yes XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
-    "example_freertos_dfu_v1           15.2.0  No  No  No  Yes XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
-    "example_freertos_dfu_v2           15.2.0  No  No  No  Yes XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
-    "example_freertos_explorer_board   15.1.0  Yes No  No  No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
-    "example_freertos_getting_started  15.1.0  No  No  No  No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
-    "example_freertos_l2_cache         15.1.0  No  Yes Yes No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
-    "example_freertos_tracealyzer      15.1.0  No  No  No  No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
-    "example_freertos_xlink_0          15.2.0  No  No  No  No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
-    "example_freertos_xlink_1          15.2.0  No  No  No  No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
-    "example_freertos_xscope_fileio    15.1.0  No  No  No  No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+    "example_freertos_audio_mux        15.1.0  No  No  No  No  No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+    "example_freertos_device_control   15.1.0  No  No  No  No  No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+    "example_freertos_dfu_v1           15.1.3  No  No  No  Yes Yes XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+    "example_freertos_dfu_v2           15.1.3  No  No  No  Yes No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+    "example_freertos_dfu_v3           15.1.3  No  No  No  Yes No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+    "example_freertos_dfu_v1           15.2.0  No  No  No  Yes Yes XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+    "example_freertos_dfu_v2           15.2.0  No  No  No  Yes No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+    "example_freertos_dfu_v3           15.2.0  No  No  No  Yes No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+    "example_freertos_explorer_board   15.1.0  Yes No  No  No  No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+    "example_freertos_getting_started  15.1.0  No  No  No  No  No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+    "example_freertos_l2_cache         15.1.0  No  Yes Yes No  No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+    "example_freertos_tracealyzer      15.1.0  No  No  No  No  No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+    "example_freertos_xlink_0          15.2.0  No  No  No  No  No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+    "example_freertos_xlink_1          15.2.0  No  No  No  No  No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
+    "example_freertos_xscope_fileio    15.1.0  No  No  No  No  No  XCORE-AI-EXPLORER  xmos_cmake_toolchain/xs3a.cmake"
 )
 
 # perform builds
@@ -42,8 +44,9 @@ for ((i = 0; i < ${#applications[@]}; i += 1)); do
     run_swmem_target="${FIELDS[3]}"
     run_datapartition_target="${FIELDS[4]}"
     run_upgrade_img_target="${FIELDS[5]}"
-    board="${FIELDS[6]}"
-    toolchain_file="${XCORE_IOT_ROOT}/${FIELDS[7]}"
+    run_loader_target="${FIELDS[6]}"
+    board="${FIELDS[7]}"
+    toolchain_file="${XCORE_IOT_ROOT}/${FIELDS[8]}"
     path="${XCORE_IOT_ROOT}"
 
     if check_tools_version ${min_tools_version}
@@ -82,6 +85,13 @@ for ((i = 0; i < ${#applications[@]}; i += 1)); do
             echo '======================================================'
             (cd ${path}/build_${board}; log_errors make create_upgrade_img_${app_target} -j)
             (cd ${path}/build_${board}; cp ${app_target}_upgrade.bin ${DIST_DIR})
+        fi
+        if [ "$run_loader_target" = "Yes" ]; then
+            echo '======================================================'
+            echo '= Making loader for' ${app_target}
+            echo '======================================================'
+            (cd ${path}/build_${board}; log_errors make create_loader_object_${app_target} -j)
+            (cd ${path}/build_${board}; cp ${app_target}_loader.o ${DIST_DIR})
         fi
     fi
 done
